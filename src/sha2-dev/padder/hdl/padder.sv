45d8df7308a7a9cb2b6cad38739d009ecdd80bad
Cristian Bourceanu <vasile.bourceanu@spc.ox.ac.uk>
2021-03-16 21:56:58 +0000

    codec extraction form tuser

--- src/sha2-dev/padder/hdl/padder.sv
+++ src/sha2-dev/padder/hdl/padder.sv
@@ -18,15 +18,13 @@
 // Additional Comments:
 //
 //////////////////////////////////////////////////////////////////////////////////
-
+`include "multiformats_codec.vh"
 `define HARD_WIRED
 module padder
 #(
     // AXI Stream Data Width
-    parameter C_M_AXIS_DATA_WIDTH=512,
-    parameter C_S_AXIS_DATA_WIDTH=512,
-    parameter C_M_AXIS_TUSER_WIDTH=128,
-    parameter C_S_AXIS_TUSER_WIDTH=128
+    parameter C_AXIS_DATA_WIDTH=512,
+    parameter C_AXIS_TUSER_WIDTH=128
 )
 (
 // Global Ports
@@ -34,31 +32,26 @@ input axis_aclk,
 input axis_resetn,
 
 // Master Stream Port
-output [(C_M_AXIS_DATA_WIDTH-1):0] m_axis_tdata,
-output reg [(C_M_AXIS_TUSER_WIDTH-1):0] m_axis_tuser,
+output [(C_AXIS_DATA_WIDTH-1):0] m_axis_tdata,
+output reg [(C_AXIS_TUSER_WIDTH-1):0] m_axis_tuser,
 output m_axis_tvalid,
 input m_axis_tready,
 output reg m_axis_tlast,
 
 // Slave Stream Port
-input [(C_S_AXIS_DATA_WIDTH-1):0] s_axis_tdata,
+input [(C_AXIS_DATA_WIDTH-1):0] s_axis_tdata,
 // Bits 34 and 33 of tuser represent sha_type
 // msb is 0 if SHA224/256 and 1 if SHA384/512
-input [(C_S_AXIS_TUSER_WIDTH-1):0] s_axis_tuser,
-input [((C_M_AXIS_DATA_WIDTH)/8-1):0] s_axis_tkeep,
+input [(C_AXIS_TUSER_WIDTH-1):0] s_axis_tuser,
+input [((C_AXIS_DATA_WIDTH)/8-1):0] s_axis_tkeep,
 input s_axis_tvalid,
 output wire s_axis_tready,
 input s_axis_tlast
 );
 
-localparam M_AXIS_TKEEP_WIDTH = C_M_AXIS_DATA_WIDTH/8;
-localparam DATA_BLOCK_REG_WIDTH=512;
-
 // ----- TUSER specs for identify sha_type ----
 localparam TUESR_SLOT_OFFSET = 32;
 localparam TUSER_SLOT_WIDTH = 16;
-localparam HASH_TUSER_SLOT = 0;
-localparam SHA_TUSER_OFFSET = 0;
 
 function integer log2;
     input integer number;
@@ -92,9 +85,39 @@ end
 `endif
 endfunction //big_endian
 
+// ---------- Sha identification -----------------
+// Bit 0 signifies if the codec is supported
+// Bit 1 signifies whether it is a 512 or 1024 block based sha hash. Supports sha1 and sha2
+function [1:0] sha_id;
+    input [C_AXIS_TUSER_WIDTH-1:0] tuser;
+    reg [15:0] codec;
+    begin
+        if(tuser[`CODEC_POS + 7: `CODEC_POS] >= 8'h80) 
+            codec = {tuser[`CODEC_POS+7: `CODEC_POS],tuser[`CODEC_POS+15: `CODEC_POS+8]};
+        else
+            codec = tuser[`CODEC_POS + 15: `CODEC_POS];
+        case(codec)
+            `CODEC_SHA1:        sha_id = 2'b01;
+            `CODEC_MD5:         sha_id = 2'b01;
+            `CODEC_MD4:         sha_id = 2'b01;
+            `CODEC_RIPEMD_128:  sha_id = 2'b01;
+            `CODEC_RIPEMD_160:  sha_id = 2'b01;
+            `CODEC_RIPEMD_256:  sha_id = 2'b01;
+            `CODEC_RIPEMD_320:  sha_id = 2'b01;
+            `CODEC_SHA2_224:    sha_id = 2'b01;
+            `CODEC_SHA2_256:    sha_id = 2'b01;
+            `CODEC_SHA2_384:    sha_id = 2'b11;
+            `CODEC_SHA2_512:    sha_id = 2'b11;
+            default:            sha_id = 2'b00;
+        endcase
+    end
+
+endfunction
+
 // ---------- Internal Parameters ------------------
-localparam C_S_AXIS_TKEEP_WIDTH = C_M_AXIS_DATA_WIDTH/8;
-localparam NUM_BYTES_WIDTH = log2(C_S_AXIS_TKEEP_WIDTH);
+localparam AXIS_TKEEP_WIDTH = C_AXIS_DATA_WIDTH/8;
+localparam DATA_BLOCK_REG_WIDTH=512;
+localparam NUM_BYTES_WIDTH = log2(AXIS_TKEEP_WIDTH);
 localparam LEN_FIELD_BYTES = 8;
 localparam LEN_FIELD_WIDTH = 8 * LEN_FIELD_BYTES;
 
@@ -138,10 +161,8 @@ end
 
 
 // ----------- Logic -----------
-assign sha_type = bom   ? s_axis_tuser[TUSER_SLOT_WIDTH*HASH_TUSER_SLOT+TUESR_SLOT_OFFSET+SHA_TUSER_OFFSET+1:
-                                TUSER_SLOT_WIDTH*HASH_TUSER_SLOT+TUESR_SLOT_OFFSET+SHA_TUSER_OFFSET] 
-                        : m_axis_tuser[TUSER_SLOT_WIDTH*HASH_TUSER_SLOT+TUESR_SLOT_OFFSET+SHA_TUSER_OFFSET+1:
-                                TUSER_SLOT_WIDTH*HASH_TUSER_SLOT+TUESR_SLOT_OFFSET+SHA_TUSER_OFFSET];
+// Identify sha_type
+assign sha_type = bom   ? sha_id(s_axis_tuser) : sha_id(m_axis_tuser);
 // Transmitting padded message block
 assign m_axis_tvalid = sha_type[1] ? reg_status[1] : reg_status[0];
 assign m_axis_tdata = sha_type[1] ? L_reg : R_reg;
@@ -170,17 +191,85 @@ assign complete = ~reg_status_actual[0] & (reg_count | ~sha_type[1])
 
 
 // ---------- Decode TKEEP ------
-/* verilator lint_off LATCH */
-genvar by;
-generate
-    for(by=0;by<M_AXIS_TKEEP_WIDTH;by=by+1) begin
-        always @(*) begin
-            if(s_axis_tkeep == {{(M_AXIS_TKEEP_WIDTH-by-1){1'b0}},{(by+1){1'b1}}})
-                last_valid_byte = by;
-        end
-    end
-endgenerate
-/* verilator lint_on LATCH */
+// genvar by;
+// generate
+//     for(by=0;by<AXIS_TKEEP_WIDTH;by=by+1) begin
+//         always_latch begin
+//             if(s_axis_tkeep == {{(AXIS_TKEEP_WIDTH-by-1){1'b0}},{(by+1){1'b1}}})
+//                 last_valid_byte = by;
+//         end
+//     end
+// endgenerate
+always @(*) begin
+    // encode FIFO IN (8b->4b)
+          case (s_axis_tkeep)
+              64'h1                : last_valid_byte = 6'h0;
+              64'h3                : last_valid_byte = 6'h1;
+              64'h7                : last_valid_byte = 6'h2;
+              64'hF                : last_valid_byte = 6'h3;
+              64'h1F               : last_valid_byte = 6'h4;
+              64'h3F               : last_valid_byte = 6'h5;
+              64'h7F               : last_valid_byte = 6'h6;
+              64'hFF               : last_valid_byte = 6'h7;
+              64'h1FF              : last_valid_byte = 6'h8;
+              64'h3FF              : last_valid_byte = 6'h9;
+              64'h7FF              : last_valid_byte = 6'ha;
+              64'hFFF              : last_valid_byte = 6'hb;
+              64'h1FFF             : last_valid_byte = 6'hc;
+              64'h3FFF             : last_valid_byte = 6'hd;
+              64'h7FFF             : last_valid_byte = 6'he;
+              64'hFFFF             : last_valid_byte = 6'hf;
+              64'h1FFFF            : last_valid_byte = 6'h10;
+              64'h3FFFF            : last_valid_byte = 6'h11;
+              64'h7FFFF            : last_valid_byte = 6'h12;
+              64'hFFFFF            : last_valid_byte = 6'h13;
+              64'h1FFFFF           : last_valid_byte = 6'h14;
+              64'h3FFFFF           : last_valid_byte = 6'h15;
+              64'h7FFFFF           : last_valid_byte = 6'h16;
+              64'hFFFFFF           : last_valid_byte = 6'h17;
+              64'h1FFFFFF          : last_valid_byte = 6'h18;
+              64'h3FFFFFF          : last_valid_byte = 6'h19;
+              64'h7FFFFFF          : last_valid_byte = 6'h1a;
+              64'hFFFFFFF          : last_valid_byte = 6'h1b;
+              64'h1FFFFFFF         : last_valid_byte = 6'h1c;
+              64'h3FFFFFFF         : last_valid_byte = 6'h1d;
+              64'h7FFFFFFF         : last_valid_byte = 6'h1e;
+              64'hFFFFFFFF         : last_valid_byte = 6'h1f;
+              64'h1_FFFFFFFF       : last_valid_byte = 6'h20;
+              64'h3_FFFFFFFF       : last_valid_byte = 6'h21;
+              64'h7_FFFFFFFF       : last_valid_byte = 6'h22;
+              64'hF_FFFFFFFF       : last_valid_byte = 6'h23;
+              64'h1F_FFFFFFFF      : last_valid_byte = 6'h24;
+              64'h3F_FFFFFFFF      : last_valid_byte = 6'h25;
+              64'h7F_FFFFFFFF      : last_valid_byte = 6'h26;
+              64'hFF_FFFFFFFF      : last_valid_byte = 6'h27;
+              64'h1FF_FFFFFFFF     : last_valid_byte = 6'h28;
+              64'h3FF_FFFFFFFF     : last_valid_byte = 6'h29;
+              64'h7FF_FFFFFFFF     : last_valid_byte = 6'h2a;
+              64'hFFF_FFFFFFFF     : last_valid_byte = 6'h2b;
+              64'h1FFF_FFFFFFFF    : last_valid_byte = 6'h2c;
+              64'h3FFF_FFFFFFFF    : last_valid_byte = 6'h2d;
+              64'h7FFF_FFFFFFFF    : last_valid_byte = 6'h2e;
+              64'hFFFF_FFFFFFFF    : last_valid_byte = 6'h2f;
+              64'h1FFFF_FFFFFFFF   : last_valid_byte = 6'h30;
+              64'h3FFFF_FFFFFFFF   : last_valid_byte = 6'h31;
+              64'h7FFFF_FFFFFFFF   : last_valid_byte = 6'h32;
+              64'hFFFFF_FFFFFFFF   : last_valid_byte = 6'h33;
+              64'h1FFFFF_FFFFFFFF  : last_valid_byte = 6'h34;
+              64'h3FFFFF_FFFFFFFF  : last_valid_byte = 6'h35;
+              64'h7FFFFF_FFFFFFFF  : last_valid_byte = 6'h36;
+              64'hFFFFFF_FFFFFFFF  : last_valid_byte = 6'h37;
+              64'h1FFFFFF_FFFFFFFF : last_valid_byte = 6'h38;
+              64'h3FFFFFF_FFFFFFFF : last_valid_byte = 6'h39;
+              64'h7FFFFFF_FFFFFFFF : last_valid_byte = 6'h3a;
+              64'hFFFFFFF_FFFFFFFF : last_valid_byte = 6'h3b;
+              64'h1FFFFFFF_FFFFFFFF: last_valid_byte = 6'h3c;
+              64'h3FFFFFFF_FFFFFFFF: last_valid_byte = 6'h3d;
+              64'h7FFFFFFF_FFFFFFFF: last_valid_byte = 6'h3e;
+              64'hFFFFFFFF_FFFFFFFF: last_valid_byte = 6'h3f;
+              default:  last_valid_byte = 6'h00;
+          endcase
+end
 
 // ---------- FSM --------------
 //FSM registers
